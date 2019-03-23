c1b7900c3faad404633664b22edc0afca4ecc93d
yiiouo <15625979610@163.com>
2019-03-23 21:29:58 +0800

    feat: test changelog

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,7 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    // Place your rules here
+    'scope-enum': [2, 'always', ['all', 'b']] // scope，规定范围只能在数组内的文案
+  }
+}
